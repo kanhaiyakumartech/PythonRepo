fe035aa7b492013f7657a6c84b9e3640ab361979
kanhaiyakumartech <kanhaiyakumar@38937@gmail.com>
2023-11-22 20:59:29 +0530

    First Question List

---
+++ src/Assignment_2/util.py
@@ -0,0 +1,9 @@
+def sum_of_marks(d, student_name):
+    for name, scores in d.items():
+        if student_name == name:
+            percentage = sum(scores) / len(scores)
+            print("The average percentage for {} is: {:.2f}%".format(name, percentage))
+            return percentage
+
+    print("Student with name {} not found.".format(student_name))
+    return None
