88f932688ec9575194c4f5dd942aa19cbfab7242
kanhaiyakumartech <150261209+kanhaiyakumartech@users.noreply.github.com>
2023-11-23 18:32:52 +0530

    code for Change the character in string

--- src/Assignment_5/driver.py
+++ src/Assignment_5/driver.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
     month, day, year = map(int, input("Enter month, day, and year (separated by space): ").split())
     result = Check_Day_Name(month, day, year)
     print(f"The day is: {result}")
+    
